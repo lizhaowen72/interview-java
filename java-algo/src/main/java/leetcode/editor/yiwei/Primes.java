ca625851a9f5efb2e8901aead83f533cee1b3b40
lizhaowen <zhaowen.li@bizgo.com.cn>
2020-06-19 18:03:46 +0800

    高效查找素数2

--- java-algo/src/main/java/leetcode/editor/yiwei/Primes.java
+++ java-algo/src/main/java/leetcode/editor/yiwei/Primes.java
@@ -7,7 +7,20 @@ import java.util.Arrays;
  * 如果一个数只能被1和它自身整除,那么这个数就是素数,1不是素数
  */
 public class Primes {
-    public static int countPrimes(int n) {
+    public static  int countPrime(int n){
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime,true);
+        for (int i = 2; i < n ; i++) {
+            if (isPrime[i]){
+                // i的倍数不可能是素数了
+                for (int j = 2*i; j < n ; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+
+    public static int countPrimes2(int n) {
         boolean[] isPrime = new boolean[n];
         Arrays.fill(isPrime, true);
         for (int i = 2; i * i < n; i++) {
@@ -27,7 +40,7 @@ public class Primes {
     }
 
     public static void main(String[] args) {
-        int counts = countPrimes(10);
+        int counts = countPrimes2(10);
         System.out.println(counts);
     }
 }
