9f738f1be2c79da4553380829462b471ba9f3765
lizhaowen72 <794112629@qq.com>
2020-06-27 22:41:23 +0800

    java性能优化34 mq

---
+++ java-mq/src/test/java/com/lzw/java/mq/JavaMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzw.java.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
